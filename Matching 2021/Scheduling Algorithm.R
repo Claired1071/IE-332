99885ce74d6a5a5fbf4be9c0ebfcfbc86188fafc
aadibyaz <78580977+aadibyaz@users.noreply.github.com>
2021-04-01 13:50:39 -0400

    Update Scheduling Algorithm.R

--- Matching 2021/Scheduling Algorithm.R
+++ Matching 2021/Scheduling Algorithm.R
@@ -3,15 +3,39 @@ ObjFunc <- function(IdealFreetimeHr,IdealStudyHr,IdealWorkHr,ActualFreetimeHr,Ac
     ((IdealWorkHr/TotalHr)*(IdealWorkHr-ActualWorkHr)) +
     ((IdealFreetimeHr/TotalHr)*(IdealFreetimeHr-ActualFreetimeHr))
 }
+
+require("RMySQL")
+library(RMySQL)
+mydb <- dbConnect(MySQL(), user = 'g1117490', password = 'iegroup9', dbname = 'g1117490', host = 'mydb.itap.purdue.edu')
+on.exit(dbDisconnect(mydb))
+
+S_ID <- 1111111111
+
+sql <- paste0("SELECT Courses FROM Student WHERE Student_ID='",S_ID,"'")
+
+Courses <- fetch(dbSendQuery(mydb,sql),n=-1)
+Course_List <- strsplit(Courses[1,1],"\r\n")
+Course_List <- sub("\r\n"," , ",Courses)
+sql <- paste0("SELECT * FROM Course WHERE Course_ID='IE33200-001'")
+#sql2 <- sprintf("SELECT * FROM Course WHERE Course_ID = 'IE33200-001'")
+sql2 <- sprintf("SELECT * FROM Course WHERE Course_ID = %s", Course_List)
+
+Class_Times <- fetch(dbSendQuery(mydb,sql2),n=-1)
+
+all_cons <- dbListConnections(MySQL())
+for (mydb in all_cons)
+  dbDisconnect(mydb)
+
 Schedule <- as.data.frame(matrix('Empty', ncol = 110, nrow = 24))
 IdealWorkHr <- 7
 IdealFreetimeHr <- 60
 IdealStudyHr <- 40
-SleepHr <- c(1:8)
+SleepHr <- c(0:8)
 ActualWorkHr <- 0
 ActualStudyHr <- 0
 ActualFreetimeHr <- 0
 TotalHr <- sum(IdealFreetimeHr,IdealStudyHr,IdealWorkHr)
+
 
 Schedule[SleepHr,] <- 'Sleep'
 
