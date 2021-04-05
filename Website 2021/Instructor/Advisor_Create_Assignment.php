684db037d734ed052dab338eb4517df53e3ea956
alvin19980205 <79758511+alvin19980205@users.noreply.github.com>
2021-04-05 11:45:12 -0400

    Update Advisor_Create_Assignment.php

--- Website 2021/Instructor/Advisor_Create_Assignment.php
+++ Website 2021/Instructor/Advisor_Create_Assignment.php
@@ -25,6 +25,7 @@
       <p>Create Assignment</p>
       <!-- creating Assignment form-->
       <form action = "Advisor_Create_Assignment.php" method="post">
+      <p><strong>Course ID: </strong> <input type="text" name = "Course_ID" placeholder="Enter Course ID" Required></p>
       <p><strong>Assignment Title: </strong> <input type="text" name = "Assignment_Name" placeholder="Enter Assignment Title" Required></p>
       <p><strong>Assignment Due Date: </strong> <input type="date" name = "Due_Date" placeholder="Enter Assignment Due Date" Required></p>
       <p><strong>Assignment Due Time: </strong> <input type="time" name = "Due_Time" placeholder="Enter Assignment Due Time" Required></p>
