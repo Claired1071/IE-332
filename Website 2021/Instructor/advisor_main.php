a7ad77326e30fa24fa5278c1f28418eb264edefa
Claired1071 <71730721+Claired1071@users.noreply.github.com>
2021-04-05 14:59:23 -0400

    Update advisor_main.php

--- Website 2021/Instructor/advisor_main.php
+++ Website 2021/Instructor/advisor_main.php
@@ -13,30 +13,28 @@ if ($_SESSION["log"] == 0) {
 <head>
 	<title>Student Scheduler</title>
     <link rel="icon" type="image/ico" href="https://web.ics.purdue.edu/~g1117490/main/ie.ico"/>
-    <link href="Advisor_Create_Profile_css.css" rel="stylesheet" type="text/css" />
+    <link href="advisor_create_profile_css.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
     <header class="h1">
       <div class="left-side">
-        <img class="banner3" src="https://www.cco.purdue.edu/Content/Layout/logo.svg" style= "width:250px">
+        <img class="banner3" src="https://image.flaticon.com/icons/png/512/123/123392.png" style= "width:250px">
         <hr class="divider">
         <div class="page-title">
           <p class="title-main">Student Scheduler</p>
           <p class="title">Redefining student scheduling</p>
         </div>
       </div>
-      <img class="banner1" src="https://cdn.shopify.com/s/files/1/0241/9737/products/1008-PUR-Tank-black_2_1800x.jpg?v=1571442802" style= "width:100px">
+      <img class="banner1" src="https://image.flaticon.com/icons/png/512/60/60785.png" style= "width:100px">
     </header>
 	
 	<!-- buttons to navigate advisor pages-->
 	<h1>Main Page</h1>
     <div id = "sideR">
-	<p><a href = "Advisor_Edit_Profile.php" class = "button1">Profile Page</a></p>
-	<p><a href = "Advisor_Create_Exam.php" class = "button1">Create Exam</a></p>
-	<p><a href = "Advisor_Edit_Exam.php" class = "button1">Change Exam</a></p>
-	<p><a href = "Advisor_Create_Assignment.php" class = "button1">Create Assignment</a></p>
-	<p><a href = "Advisor_Edit_Assignment.php" class = "button1">Change Assignment</a></p>
-	<p><a href = "Advisor_Logout.php" class = "button1">Logout</a></p>
+	<p><a href = "advisor_profile.php" class = "button1">Profile Page</a></p>
+	<p><a href = "advisor_exam.php" class = "button1">Create Exam</a></p>
+	<p><a href = "advisor_assignment.php" class = "button1">Create Assignment</a></p>
+	<p><a href = "advisor_logout.php" class = "button1">Logout</a></p>
 	<br>
 </body>
 </html>
