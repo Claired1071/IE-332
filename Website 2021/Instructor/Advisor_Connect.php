9b66e5296345de8ad432719761d92a9427226b6a
alvin19980205 <79758511+alvin19980205@users.noreply.github.com>
2021-04-05 12:17:08 -0400

    Create Advisor_Connect.php

---
+++ Website 2021/Instructor/Advisor_Connect.php
@@ -0,0 +1,16 @@
+<?php
+
+//Establishing connection to MySQL database
+$servername = "mydb.itap.purdue.edu";
+$username = "g1117490";
+$password = "$pring21G9!";
+$dbname = "g1117490";
+
+$data_base = mysqli_connect($servername, $username, $password, $dbname);
+
+if(!$data_base){
+die("Connection failed: " . mysqli_connect_error());
+}
+// echo "Connection established";
+
+?>
