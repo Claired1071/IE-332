064d735ad43e54908214de3829965466b98a78b2
Claired1071 <71730721+Claired1071@users.noreply.github.com>
2021-04-05 14:45:17 -0400

    Update and rename Student_Edit_Profile.php to student_edit_profile.php

--- Website 2021/Student/student_edit_profile.php
+++ Website 2021/Student/student_edit_profile.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-include("Student_Connect.php");
+include("student_connect.php");
 
 //user cannot go back after logging out
 if ($_SESSION["active"] == 0) {
-	header("Location:  Student_Login.php");
+	header("Location:  student_login.php");
 }
 
 ?>
@@ -16,19 +16,19 @@ if ($_SESSION["active"] == 0) {
 <head>
 	<title>Student Scheduler</title>
     <link rel="icon" type="image/ico" href="https://web.ics.purdue.edu/~g1116905/main/ie.ico"/>
-    <link href="Student_Create_Profile_css.css" rel="stylesheet" type="text/css" />
+    <link href="student_create_profile_css.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
     <header class="h1">
       <div class="left-side">
-        <img class="banner3" src="https://www.cco.purdue.edu/Content/Layout/logo.svg" style= "width:250px">
+        <img class="banner3" src="https://image.flaticon.com/icons/png/512/123/123392.png" style= "width:250px">
         <hr class="divider">
         <div class="page-title">
           <p class="title-main">Student Scheduler</p>
           <p class="title">Redefining student scheduling</p>
         </div>
       </div>
-      <img class="banner1" src="https://cdn.shopify.com/s/files/1/0241/9737/products/1008-PUR-Tank-black_2_1800x.jpg?v=1571442802" style= "width:100px">
+      <img class="banner1" src="https://image.flaticon.com/icons/png/512/60/60785.png" style= "width:100px">
     </header>
 
 <h1>Edit Profile</h1>
@@ -42,7 +42,7 @@ $result = mysqli_query($data_base,$query);
 if (mysqli_num_rows($result) > 0) {
 ?>
 
-<form action = "Student_Edit_Profile_Confirmation" method="post">
+<form action = "student_edit_profile_confirmation" method="post">
 	
     <?php
     while ($row = mysqli_fetch_array($result)) {
@@ -52,7 +52,6 @@ if (mysqli_num_rows($result) > 0) {
 	<p><strong>Password: </strong><input type = "password" name = "Password" value = "<?php echo $row["Password"]; ?>" Required></p>
 	<p><strong>First Name: </strong><input type = "text" name = "Fname" value = "<?php echo $row["Fname"]; ?>" Required></p>
 	<p><strong>Last Name: </strong><input type = "text" name = "Lname" value = "<?php echo $row["Lname"]; ?>" Required></p>
-	<p><strong>Phone Number (area code first): </strong><input type = "number" name = "Pnumber" value = "<?php echo $row["Pnumber"]; ?>" Required></p>
         <p><strong>Major: </strong><input type="text" name = "Major" placeholder="Enter Major" value = "<?php echo $row["Major"]; ?>" Required></p>
 	<p><strong>GPA: </strong><input type="text" name = "GPA" placeholder="Enter GPA" value = "<?php echo $row["GPA"]; ?>" Required></p>
 	<p><strong>Courses: </strong><input type="text" name = "Courses" value = "<?php echo $row["Courses"]; ?>" Required></p>
@@ -65,7 +64,7 @@ if (mysqli_num_rows($result) > 0) {
 }
 mysqli_close($data_base);
 ?>
-<p><a href="Student_Main.php" class = "button1">Main Page</a></p>
+<p><a href="student_main.php" class = "button1">Main Page</a></p>
 
 </body>
 </html>
