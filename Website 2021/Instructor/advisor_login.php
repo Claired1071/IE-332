8d1b60ae7ecdb8f564dfae23771472fc0e5bc6ed
Claired1071 <71730721+Claired1071@users.noreply.github.com>
2021-04-05 15:01:38 -0400

    Update advisor_login.php

--- Website 2021/Instructor/advisor_login.php
+++ Website 2021/Instructor/advisor_login.php
@@ -2,7 +2,7 @@
 session_start();
 
 if ($_SESSION["active"] == 1) {
-	header("Location: student_main.php");
+	header("Location: advisor_main.php");
 }
 $message = "";
 if(count($_POST)>0) {
@@ -11,7 +11,7 @@ if(count($_POST)>0) {
 	$password = "$ie332group9!";
 	$dbname = "g1117490";	
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
-	$result = mysqli_query($conn,"SELECT * FROM Advisor WHERE Email='" . $_POST["s_email"] . "' AND Password = '". $_POST["s_password"]."'");
+	$result = mysqli_query($conn,"SELECT * FROM Advisor WHERE Email='" . $_POST["a_email"] . "' AND Password = '". $_POST["a_password"]."'");
 	$count  = mysqli_num_rows($result);
 	if($count==0) {
 		$message = "Incorrect username or password";
@@ -20,7 +20,7 @@ if(count($_POST)>0) {
 		
 		$_SESSION["log"] = 1;
 
-		$_SESSION["s_email"] =  $_POST["s_email"];
+		$_SESSION["a_email"] =  $_POST["a_email"];
 	}
 	mysqli_close($conn);
 }
@@ -34,23 +34,23 @@ if(count($_POST)>0) {
 		<link href="login.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
-		<img class="logo" src="https://wl.mypurdue.purdue.edu/static_resources/portal/images/logo.png">
+		<img class="logo" src="https://image.flaticon.com/icons/png/512/123/123392.png">
 		<div class="login-container">
 			<h2 class="title">Hello!</h2>
 			<form action="" method = "post">
 				<div class="form">
-					<label for="s_email">Email:</label>
-				    <input class="input" type="email" id="s_email" name="s_email" placeholder="Email address" required>
+					<label for="a_email">Email:</label>
+				    <input class="input" type="email" id="a_email" name="a_email" placeholder="Email address" required>
 				</div>
 			    <br>
 			    <div class="form">
 			    	<label for="s_pass">Password:</label>
-				    <input class="input" type="password" id="s_pass" name="s_pass" placeholder="Password" required>
+				    <input class="input" type="password" id="a_pass" name="a_pass" placeholder="Password" required>
 					<input type="checkbox" onclick="myFunction()">Show Password
 					<script>
 					<!-- java function for showing password after checking box-->
 					function myFunction() {
-						var x = document.getElementById("stud_password");
+						var x = document.getElementById("a_password");
 						if (x.type === "password") {
 							x.type = "text";
 						} else {
@@ -66,7 +66,7 @@ if(count($_POST)>0) {
 			</form>
 			<p> New user? Create an account below!</p>
 			<p><a class="new-account" href="https://web.ics.purdue.edu/~g1117490/main/advisor_create_profile.php" target="_self" >Create Account</a></p>
-			<p><a class="new-account" href="https://web.ics.purdue.edu/~g1117490/main/advisor_mainpage.php" target="_self" >Home Page</a></p>
+			<p><a class="new-account" href="https://web.ics.purdue.edu/~g1117490/main/advisor_main.php" target="_self" >Home Page</a></p>
 		</div>
 	</body>
 </html>
