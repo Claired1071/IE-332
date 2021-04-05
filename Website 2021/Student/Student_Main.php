aa00d09e9a7a5d48bb6bc94f4d93515482665f7e
Claired1071 <71730721+Claired1071@users.noreply.github.com>
2021-04-05 13:27:09 -0400

    Update Student_Main.php

--- Website 2021/Student/Student_Main.php
+++ Website 2021/Student/Student_Main.php
@@ -1,37 +1,29 @@
-<?php
+<!--?php 
 session_start();
 //user cannot go back after logging out
 if ($_SESSION["active"] == 0) {
 	header("Location:  student_login.php");
 }
-?>
-<!DOCTYPE html>
-<html>
+?-->
+<p>&nbsp;</p>
+<p>&nbsp;</p>
 <!-- importing css from external file-->
-<head>
-	<title>Student Scheduler</title>
-    <link rel="icon" type="image/ico" href="https://web.ics.purdue.edu/~g1117490/main/ie.ico"/>
-    <link href="Student_Create_Profile_css" rel="stylesheet" type="text/css" />
-</head>
-<body>
-    <header class="h1">
-      <div class="left-side">
-        <img class="banner3" src="https://www.cco.purdue.edu/Content/Layout/logo.svg" style= "width:250px">
-        <hr class="divider">
-        <div class="page-title">
-          <p class="title-main">Student Scheduler</p>
-          <p class="title">Redefining student scheduling</p>
-        </div>
-      </div>
-      <img class="banner1" src="https://cdn.shopify.com/s/files/1/0241/9737/products/1008-PUR-Tank-black_2_1800x.jpg?v=1571442802" style= "width:100px">
-    </header>
-	
-	<!-- buttons to go to other pages-->
-	<h1>Main</h1>
-    <div id = "sideR">
-	<p><a href = "Student_Profile.php" class = "button1">Profile</a></p>
-	<p><a href = "Student_Logout.php" class = "button1">Logout</a></p>
-	<br>
-	<p><i>Thank you for using Student Scheduler!</i></p>
-</body>
-</html>
+<p>&nbsp;</p>
+<p></p>
+<header class="h1">
+<div class="left-side"><img class="banner3" style="width: 250px;" src="https://image.flaticon.com/icons/png/512/123/123392.png" /><hr class="divider" />
+<div class="page-title">
+<p class="title-main">Student Scheduler</p>
+<p class="title">Redefining student scheduling</p>
+</div>
+</div>
+<img class="banner1" style="width: 100px;" src="https://image.flaticon.com/icons/png/512/60/60785.png" /></header><!-- buttons to go to other pages-->
+<h1>Main</h1>
+<div id="sideR">
+<p><a class="button1" href="Student_Profile.php">Profile</a></p>
+  <p><a class="button1" href="Student_Events.php">Events</a></p>
+  <p><a class="button1" href="Student_Feedback.php">Feedback</a></p>
+<p><a class="button1" href="Student_Logout.php">Logout</a></p>
+<br />
+<p><em>Thank you for using Student Scheduler!</em></p>
+</div>
