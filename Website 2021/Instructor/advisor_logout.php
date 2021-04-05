41b0b87993b86ef53d434e8d9907e4db99966d78
Claired1071 <71730721+Claired1071@users.noreply.github.com>
2021-04-05 14:59:48 -0400

    Update advisor_logout.php

--- Website 2021/Instructor/advisor_logout.php
+++ Website 2021/Instructor/advisor_logout.php
@@ -3,6 +3,6 @@ session_start();
 
 //setting variable to use in all other pages so student cannot use back arrow in browser after logging out
 $_SESSION["log"] = 0;
-unset($_SESSION["s_email"]);
+unset($_SESSION["a_email"]);
 header("Location: advisor_login.php");
 ?>
