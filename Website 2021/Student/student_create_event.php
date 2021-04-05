6ee8b4a5296c78bdff229a4fa0f3be189c07a38b
Claired1071 <71730721+Claired1071@users.noreply.github.com>
2021-04-05 14:22:38 -0400

    Update and rename Student_Create_Event.php to student_create_event.php

--- Website 2021/Student/student_create_event.php
+++ Website 2021/Student/student_create_event.php
@@ -6,7 +6,7 @@
 <head>
 	<title>Student Scheduler</title>
     <link rel="icon" type="image/ico" href="https://web.ics.purdue.edu/~g1117490/main/ie.ico"/>
-    <link href="Student_Create_Profile_css.css" rel="stylesheet" type="text/css" />
+    <link href="student_create_profile_css.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
     <header class="h1">
@@ -24,7 +24,7 @@
 
       <p>Create Event</p>
       <!-- creating student event form-->
-      <form action = "Student_Create_Event.php" method="post">
+      <form action = "student_create_event.php" method="post">
       <p><strong>Event Title: </strong> <input type="text" name = "Etitle" placeholder="Enter Event Title" Required></p>
       <p><strong>Event Date: </strong> <input type="date" name = "Edate" placeholder="Enter Event Date" Required></p>
       <p><strong>Event Time: </strong> <input type="time" name = "Etime" placeholder="Enter Event Time" Required></p>
