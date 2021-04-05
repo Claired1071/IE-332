b48cdff94441411ebe42accc500fa12c1638b437
Claired1071 <71730721+Claired1071@users.noreply.github.com>
2021-04-05 15:02:49 -0400

    Update advisor_feedback.php

--- Website 2021/Instructor/advisor_feedback.php
+++ Website 2021/Instructor/advisor_feedback.php
@@ -11,14 +11,14 @@
 <body>
     <header class="h1">
       <div class="left-side">
-        <img class="banner3" src="https://www.cco.purdue.edu/Content/Layout/logo.svg" style= "width:250px">
+        <img class="banner3" src="https://image.flaticon.com/icons/png/512/123/123392.png" style= "width:250px">
         <hr class="divider">
         <div class="page-title">
           <p class="title-main">Student Scheduler</p>
           <p class="title">Redefining student scheduling</p>
         </div>
       </div>
-      <img class="banner1" src="https://cdn.shopify.com/s/files/1/0241/9737/products/1008-PUR-Tank-black_2_1800x.jpg?v=1571442802" style= "width:100px">
+      <img class="banner1" src="https://image.flaticon.com/icons/png/512/60/60785.png" style= "width:100px">
     </header>
     <h1> Feedback </h1>
     <p> Feedback </p>
