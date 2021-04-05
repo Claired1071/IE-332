3daff3d615d0df221734c6c719df54b1986f6f2e
Claired1071 <71730721+Claired1071@users.noreply.github.com>
2021-04-05 14:52:30 -0400

    Update and rename Student_Logout.php to student_logout.php

--- Website 2021/Student/student_logout.php
+++ Website 2021/Student/student_logout.php
@@ -3,5 +3,5 @@ session_start();
 //user is not allowed to go back into the browser after logging out
 $_SESSION["active"] = 0;
 unset($_SESSION["s_email"]);
-header("Location: Student_Login.php");
+header("Location: student_login.php");
 ?>
