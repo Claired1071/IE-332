c722450222434400c558638a26b1b7793c48ef41
Claired1071 <71730721+Claired1071@users.noreply.github.com>
2021-04-10 16:50:02 -0400

    Update student_create_event.php

--- Website 2021/Student/student_create_event.php
+++ Website 2021/Student/student_create_event.php
@@ -10,14 +10,15 @@
 <p class="title">Redefining student scheduling</p>
 </div>
 </div>
-<img class="banner1" style="width: 100px;" src="https://image.flaticon.com/icons/png/512/60/60785.png" /></header>
+<img class="banner1" style="width: 100px;" src="https://www.flaticon.com/svg/vstatic/svg/4185/4185823.svg?token=exp=1618088399~hmac=85714128e97e4f96030e0e83ec09bc99" /></header>
 <h1>Create an Event</h1>
 <p>Create Event</p>
 <!-- creating student event form--><form action="student_create_event.php" method="post">
-<p><strong>Event Title: </strong> <input name="Etitle" required="" type="text" placeholder="Enter Event Title" /></p>
-<p><strong>Event Date: </strong> <input name="Edate" required="" type="date" placeholder="Enter Event Date" /></p>
-<p><strong>Event Time: </strong> <input name="Etime" required="" type="time" placeholder="Enter Event Time" /></p>
-<p><strong>Event Type:</strong><select name="Etype">
+<p><strong>Event Title: </strong> <input name="Event_Name" required="" type="text" placeholder="Enter Event Title" /></p>
+<p><strong>Event Date: </strong> <input name="Event_Date" required="" type="date" placeholder="Enter Event Date" /></p>
+<p><strong>Event Start Time: </strong> <input name="Event_Start_Time" required="" type="time" placeholder="Enter  Start Time" /></p>
+  <p><strong>Event End Time: </strong> <input name="Event_End_Time" required="" type="time" placeholder="Enter End Time" /></p>
+<p><strong>Event Type:</strong><select name="Event_Type">
 <option>Social</option>
 <option>Work</option>
 <option>Freetime/Other</option>
