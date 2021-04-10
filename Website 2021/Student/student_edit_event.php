c1f38e2c0e2fdae2eed3437f22961e0f98a8dc69
Claired1071 <71730721+Claired1071@users.noreply.github.com>
2021-04-10 17:12:06 -0400

    Update student_edit_event.php

--- Website 2021/Student/student_edit_event.php
+++ Website 2021/Student/student_edit_event.php
@@ -11,14 +11,14 @@
 <body>
     <header class="h1">
       <div class="left-side">
-        <img class="banner3" src="https://image.flaticon.com/icons/png/512/123/123392.png" style= "width:250px">
+        <img class="banner3" src="https://www.flaticon.com/svg/vstatic/svg/4185/4185688.svg?token=exp=1618089495~hmac=84e1937dad55bad2ee74e52fe392a686" style= "width:250px">
         <hr class="divider">
         <div class="page-title">
           <p class="title-main">Student Scheduler</p>
           <p class="title">Redefining student scheduling</p>
         </div>
       </div>
-      <img class="banner1" src="https://image.flaticon.com/icons/png/512/60/60785.png" style= "width:100px">
+      <img class="banner1" src="https://www.flaticon.com/svg/vstatic/svg/4185/4185823.svg?token=exp=1618088399~hmac=85714128e97e4f96030e0e83ec09bc99" style= "width:100px">
     </header>
     <h1> Change Event </h1>
     <p> Change Event </p>
@@ -35,12 +35,13 @@ $result = mysqli_query($data_base,$query);
         </select></p>
       <p><strong>Event Title: </strong> <input type="text" name = "Event Title" placeholder="Enter New Event Title" Required></p>
       <p><strong>Event Date: </strong> <input type="date" name = "Event Date" placeholder="Enter New Event Date" Required></p>
-      <p><strong>Event Time: </strong> <input type="time" name = "Event Time" placeholder="Enter New Event Time" Required></p>
-        <p><strong>Event Type:</strong>
+      <p><strong>Event Start Time: </strong> <input type="time" name = "Event Time" placeholder="Enter New Start Time" Required></p>
+        <p><strong>Event End Time: </strong> <input type="time" name = "Event Time" placeholder="Enter New End Time" Required></p>
+       <p><strong>Event Type:</strong>
         <select type = "text" name = "Event Type">
                 <option>Social</option>
                 <option>Work</option>
-          <option>Other</option>
+          <option>Freetime/Other</option>
         </select></p>
       <p><input type="submit" class = "button1" value="Change Event" /></p>
 	</form>
