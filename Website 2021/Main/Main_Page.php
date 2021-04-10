6ce6bccb3dd6516c70240723a8e7cc36a33a1954
Claired1071 <71730721+Claired1071@users.noreply.github.com>
2021-04-10 17:30:50 -0400

    Update Main_Page.php

--- Website 2021/Main/Main_Page.php
+++ Website 2021/Main/Main_Page.php
@@ -3,19 +3,19 @@
   <head>
     <title>Student Scheduler</title>
     <link rel="icon" type="image/ico" href="https://web.ics.purdue.edu/~g1117490/main/ie.ico"/>
-    <link href="test21.css" width = "100%" rel="stylesheet" type="text/css" />
+    <link href="student_create_profile_css.css" width = "100%" rel="stylesheet" type="text/css" />
   </head>
   <body>
     <header class="h1">
       <div class="left-side">
-        <img class="banner3" src="https://image.flaticon.com/icons/png/512/123/123392.png" style= "width:250px">
+        <img class="banner3" src="https://www.flaticon.com/svg/vstatic/svg/4185/4185688.svg?token=exp=1618089495~hmac=84e1937dad55bad2ee74e52fe392a686" style= "width:250px">
         <hr class="divider">
         <div class="page-title">
           <p class="title-main">Student Scheduler</p>
           <p class="title">Redefining student scheduling</p>
         </div>
       </div>
-      <img class="banner1" src="https://image.flaticon.com/icons/png/512/60/60785.png" style= "width:100px">
+      <img class="banner1" src="https://www.flaticon.com/svg/vstatic/svg/4185/4185823.svg?token=exp=1618088399~hmac=85714128e97e4f96030e0e83ec09bc99" style= "width:100px">
     </header>
     <div class="page">
       <img class="hand" src="https://image.freepik.com/free-vector/time-management-concept-talk-chart_23-2148831728.jpg">
@@ -26,6 +26,6 @@
         <div class="button">
           <a href="advisor/advisor_login.php" target="_self">I am an Advisor</a>
         </div>
-	<h2><p><a href="group_page.php" class = "button1">IE 332 Group 9 Members</a></p></h2>
+	<h2><p><a href="Group_Page.php" class = "button1">IE 332 Group 9 Members</a></p></h2>
   </body>
 </html>
