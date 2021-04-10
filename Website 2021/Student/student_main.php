98b51658c560c5e0d70437867cc47425d2c20514
Claired1071 <71730721+Claired1071@users.noreply.github.com>
2021-04-10 17:59:45 -0400

    Update student_main.php

--- Website 2021/Student/student_main.php
+++ Website 2021/Student/student_main.php
@@ -1,28 +1,37 @@
-<!--?php 
+<?php 
 session_start();
 //user cannot go back after logging out
 if ($_SESSION["active"] == 0) {
 	header("Location:  student_login.php");
 }
-?-->
-<p>&nbsp;</p>
-<p>&nbsp;</p>
+?>
+<!DOCTYPE html>
+<html>
 <!-- importing css from external file-->
-<p>&nbsp;</p>
-<p></p>
-<header class="h1">
-<div class="left-side"><img class="banner3" style="width: 250px;" src="https://image.flaticon.com/icons/png/512/123/123392.png" /><hr class="divider" />
-<div class="page-title">
-<p class="title-main">Student Scheduler</p>
-<p class="title">Redefining student scheduling</p>
-</div>
-</div>
-<img class="banner1" style="width: 100px;" src="https://image.flaticon.com/icons/png/512/60/60785.png" /></header><!-- buttons to go to other pages-->
+<head>
+	<title>Student Scheduler</title>
+    <link rel="icon" type="image/ico" href="https://web.ics.purdue.edu/~g1117490/main/ie.ico"/>
+    <link href="student_create_profile_css.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <header class="h1">
+      <div class="left-side">
+        <img class="banner3" src="https://www.flaticon.com/svg/vstatic/svg/4185/4185688.svg?token=exp=1618089495~hmac=84e1937dad55bad2ee74e52fe392a686" style= "width:250px">
+        <hr class="divider">
+        <div class="page-title">
+          <p class="title-main">Student Scheduler</p>
+          <p class="title">Redefining student scheduling</p>
+        </div>
+      </div>
+      <img class="banner1" src="https://www.flaticon.com/svg/vstatic/svg/4185/4185823.svg?token=exp=1618088399~hmac=85714128e97e4f96030e0e83ec09bc99" style= "width:100px">
+    </header>
+	<!-- buttons to go to other pages-->
 <h1>Main</h1>
 <div id="sideR">
 <p><a class="button1" href="student_profile.php">Profile</a></p>
-  <p><a class="button1" href="student_events.php">Events</a></p>
-  <p><a class="button1" href="student_feedback.php">Feedback</a></p>
+<p><a class="button1" href="student_events.php">Events</a></p>
+<p><a class="button1" href="student_schedule.php">Schedule</a></p>
+<p><a class="button1" href="student_feedback.php">Feedback</a></p>
 <p><a class="button1" href="student_logout.php">Logout</a></p>
 <br />
 <p><em>Thank you for using Student Scheduler!</em></p>
