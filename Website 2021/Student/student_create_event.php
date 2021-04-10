3187edf3e9d2f96b53ec94a537e973248c2fc4be
Claired1071 <71730721+Claired1071@users.noreply.github.com>
2021-04-10 15:34:40 -0400

    Update student_create_event.php

--- Website 2021/Student/student_create_event.php
+++ Website 2021/Student/student_create_event.php
@@ -1,41 +1,26 @@
-   <!DOCTYPE html>
-<html>
-
+<p>&nbsp;</p>
+<p>&nbsp;</p>
 <!-- importing css from external file-->
-
-<head>
-	<title>Student Scheduler</title>
-    <link rel="icon" type="image/ico" href="https://web.ics.purdue.edu/~g1117490/main/ie.ico"/>
-    <link href="student_create_profile_css.css" rel="stylesheet" type="text/css" />
-</head>
-<body>
-    <header class="h1">
-      <div class="left-side">
-        <img class="banner3" src="https://image.flaticon.com/icons/png/512/123/123392.png" style= "width:250px">
-        <hr class="divider">
-        <div class="page-title">
-          <p class="title-main">Student Scheduler</p>
-          <p class="title">Redefining student scheduling</p>
-        </div>
-      </div>
-      <img class="banner1" src="https://image.flaticon.com/icons/png/512/60/60785.png" style= "width:100px">
-    </header>
-    <h1> Event Creation</h1> 
-
-      <p>Create Event</p>
-      <!-- creating student event form-->
-      <form action = "student_create_event.php" method="post">
-      <p><strong>Event Title: </strong> <input type="text" name = "Etitle" placeholder="Enter Event Title" Required></p>
-      <p><strong>Event Date: </strong> <input type="date" name = "Edate" placeholder="Enter Event Date" Required></p>
-      <p><strong>Event Time: </strong> <input type="time" name = "Etime" placeholder="Enter Event Time" Required></p>
-        <p><strong>Event Type:</strong>
-        <select type = "text" name = "Etype">
-                <option>Social</option>
-                <option>Work</option>
-          <option>Other</option>
-        </select></p>
-      <p><input type="submit" class = "button1" value="Create Event" /></p>
-	</form>
-
-</body>
-</html>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<header class="h1">
+<div class="left-side"><img class="banner3" style="width: 250px;" src="https://image.flaticon.com/icons/png/512/123/123392.png" /><hr class="divider" />
+<div class="page-title">
+<p class="title-main">Student Scheduler</p>
+<p class="title">Redefining student scheduling</p>
+</div>
+</div>
+<img class="banner1" style="width: 100px;" src="https://image.flaticon.com/icons/png/512/60/60785.png" /></header>
+<h1>Create an Event</h1>
+<p>Create Event</p>
+<!-- creating student event form--><form action="student_create_event.php" method="post">
+<p><strong>Event Title: </strong> <input name="Etitle" required="" type="text" placeholder="Enter Event Title" /></p>
+<p><strong>Event Date: </strong> <input name="Edate" required="" type="date" placeholder="Enter Event Date" /></p>
+<p><strong>Event Time: </strong> <input name="Etime" required="" type="time" placeholder="Enter Event Time" /></p>
+<p><strong>Event Type:</strong><select name="Etype">
+<option>Social</option>
+<option>Work</option>
+<option>Freetime/Other</option>
+</select></p>
+<p><input class="button1" type="submit" value="Create Event" /></p>
+</form>
