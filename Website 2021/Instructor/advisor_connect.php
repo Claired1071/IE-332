d5c0e9f0f2fe255ea043579063e3a0bf4224bf72
alvin19980205 <79758511+alvin19980205@users.noreply.github.com>
2021-04-12 01:01:01 -0400

    Update advisor_connect.php

--- Website 2021/Instructor/advisor_connect.php
+++ Website 2021/Instructor/advisor_connect.php
@@ -3,7 +3,7 @@
 //Establishing connection to MySQL database
 $servername = "mydb.itap.purdue.edu";
 $username = "g1117490";
-$password = "$pring21G9!";
+$password = "iegroup9";
 $dbname = "g1117490";
 
 $data_base = mysqli_connect($servername, $username, $password, $dbname);
