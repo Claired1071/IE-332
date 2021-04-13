3d8835520929d52977f43c9a6adddd1845725b0b
Claired1071 <71730721+Claired1071@users.noreply.github.com>
2021-04-12 21:06:28 -0400

    Update Main_Page.php

--- Website 2021/Main/Main_Page.php
+++ Website 2021/Main/Main_Page.php
@@ -3,7 +3,7 @@
   <head>
     <title>Student Scheduler</title>
     <link rel="icon" type="image/ico" href="https://web.ics.purdue.edu/~g1117490/main/ie.ico"/>
-    <link href="main_css.css" width = "100%" rel="stylesheet" type="text/css" />
+    <link href="Main_css.css" rel="stylesheet" type="text/css" />
   </head>
   <body>
     <header class="h1">
@@ -21,11 +21,16 @@
       <img class="hand" src="https://www.flaticon.com/svg/vstatic/svg/4185/4185301.svg?token=exp=1618093386~hmac=a726bca543c696bb3e2496c2427e926d">
       <div class="person-options">
         <div class="button">
-          <a href = "student/student_login.php" target="_self">I am a Student</a>
+          <a href = "student/student_login.php" class="button" target="_self" style= "padding:0">Student Login</a>
         </div>
         <div class="button">
-          <a href="advisor/advisor_login.php" target="_self">I am an Advisor</a>
+          <a href="employer/advisor_login.php" class="button" target="_self" style= "padding:0">Advisor Login</a>
         </div>
-	<h2><p><a href="Group_Page.php" class = "button1">IE 332 Group 9 Members</a></p></h2>
+        <h2><p><a href="Group_Page.php" class = "button1">IE 332 Team 9 Group</a></p></h2>
+      </div>
+    </div>
+  <footer class="footer">
+    NON-AFFILIATION DISCLAIMER: This website is not endorsed by, directly affiliated with, maintained, authorized, or sponsored by Purdue University.
+  </footer>
   </body>
 </html>
