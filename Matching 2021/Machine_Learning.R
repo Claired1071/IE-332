5267d241c32601cfe1dad05bd987e9ad4d0deadb
Shailendra1811 <79381316+Shailendra1811@users.noreply.github.com>
2021-04-26 19:26:17 -0400

    Update Machine_Learning.R

--- Matching 2021/Machine_Learning.R
+++ Matching 2021/Machine_Learning.R
@@ -2,9 +2,10 @@ mydb <- dbConnect(MySQL(), user = ‘g1117490’, password = ‘xxxxxx’, dbnam
 on.exit(dbDisconnect(mydb))
 Query <- SELECT S.GPA, S.Year, S.Major, F.Feedback_morehours, F.Feedback_lesshours, A.Suggested_Study_Time
 FROM Student S, Feedback F, Assignment A
-WHERE A.Assignment_Name = F.Assignment_Name  AND S.email = F.email
+WHERE A.Assignment_Name = F.Assignment_Name AND S.email = F.email
+#Total_time <- Suggested_Study_Time + Feedback_morehours – Feedback_lesshours
 df <- dbGetQuery(mydb, Query)
-Total_time <- Suggested_Study_Time + Feedback_morehours – Feedback_lesshours
+
 Kmeans_model <- kmeans(df, cluster = 6, nstart = 10)
 aggregate(df, by = list(cluster=Kmeans_model$cluster), mean)
 
