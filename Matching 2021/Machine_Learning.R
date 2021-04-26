59a12bee03b8fdbafecb60647b2daf81e689da3e
Shailendra1811 <79381316+Shailendra1811@users.noreply.github.com>
2021-04-26 19:56:43 -0400

    Update Machine_Learning.R

--- Matching 2021/Machine_Learning.R
+++ Matching 2021/Machine_Learning.R
@@ -1,6 +1,6 @@
 mydb <- dbConnect(MySQL(), user = ‘g1117490’, password = ‘xxxxxx’, dbname = ‘g1117490’, host = ‘mydb.itap.purdue.edu’)
 on.exit(dbDisconnect(mydb))
-Query <- SELECT S.GPA, S.Year, S.Major, F.Feedback_morehours, F.Feedback_lesshours, A.Suggested_Study_Time
+Query <- SELECT (SELECT A.Suggested_Study_Time) + (SELECT F.Feedback_morehours) - (SELECT F.Feedback_lesshours) AS total_time, S.GPA, S.Year, S.Major 
 FROM Student S, Feedback F, Assignment A
 WHERE A.Assignment_Name = F.Assignment_Name AND S.email = F.email
 #Total_time <- Suggested_Study_Time + Feedback_morehours – Feedback_lesshours
@@ -8,7 +8,7 @@ df <- dbGetQuery(mydb, Query)
 
 Kmeans_model <- kmeans(df, cluster = 6, nstart = 10)
 aggregate(df, by = list(cluster=Kmeans_model$cluster), mean)
-
+Cluster_means <- Kmeans_model$centers
 
 
 
