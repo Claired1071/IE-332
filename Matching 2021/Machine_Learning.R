45f1f85db3b184c8ea5893cc5bce56eaa2b79118
Shailendra1811 <79381316+Shailendra1811@users.noreply.github.com>
2021-04-26 20:28:23 -0400

    Update Machine_Learning.R

--- Matching 2021/Machine_Learning.R
+++ Matching 2021/Machine_Learning.R
@@ -1,6 +1,18 @@
 mydb <- dbConnect(MySQL(), user = ‘g1117490’, password = ‘xxxxxx’, dbname = ‘g1117490’, host = ‘mydb.itap.purdue.edu’)
 on.exit(dbDisconnect(mydb))
-Query <- SELECT (SELECT A.Suggested_Study_Time) + (SELECT F.Feedback_morehours) - (SELECT F.Feedback_lesshours) AS total_time, S.GPA, S.Year, S.Major 
+#Query <- SELECT (SELECT A.Suggested_Study_Time) + (SELECT F.Feedback_morehours) - (SELECT F.Feedback_lesshours) AS total_time, S.GPA, S.Year, S.Major 
+
+sql <- sprintf("SELECT Suggested_Study_Time FROM Assignment WHERE Assignment_Name='%s'", Assignment_Name)
+Suggested_Time <- fetch(dbSendQuery(mydb,sql),n=-1)
+
+sql <- sprintf("SELECT Feedback_morehours FROM Feedback WHERE Assignment_Name='%s'", Assignment_Name)
+More_hours <- fetch(dbSendQuery(mydb,sql),n=-1)
+
+sql <- sprintf("SELECT Feeback_lesshours FROM Feedback WHERE Assignment_Name='%s'", Assignment_Name)
+Less_hours <- fetch(dbSendQuery(mydb,sql),n=-1)
+
+
+
 FROM Student S, Feedback F, Assignment A
 WHERE A.Assignment_Name = F.Assignment_Name AND S.email = F.email
 #Total_time <- Suggested_Study_Time + Feedback_morehours – Feedback_lesshours
