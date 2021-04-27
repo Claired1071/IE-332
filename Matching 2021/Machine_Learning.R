e2943bde9b2116e8379a8161c103826b6cede874
Shailendra1811 <79381316+Shailendra1811@users.noreply.github.com>
2021-04-27 01:35:29 -0400

    Update Machine_Learning.R

--- Matching 2021/Machine_Learning.R
+++ Matching 2021/Machine_Learning.R
@@ -1,6 +1,5 @@
 mydb <- dbConnect(MySQL(), user = ‘g1117490’, password = ‘xxxxxx’, dbname = ‘g1117490’, host = ‘mydb.itap.purdue.edu’)
 on.exit(dbDisconnect(mydb))
-#Query <- SELECT (SELECT A.Suggested_Study_Time) + (SELECT F.Feedback_morehours) - (SELECT F.Feedback_lesshours) AS total_time, S.GPA, S.Year, S.Major 
 
 sql <- sprintf("SELECT Suggested_Study_Time FROM Assignment WHERE Assignment_Name='%s'", Assignment_Name)
 Suggested_Time <- fetch(dbSendQuery(mydb,sql),n=-1)
@@ -11,11 +10,16 @@ More_hours <- fetch(dbSendQuery(mydb,sql),n=-1)
 sql <- sprintf("SELECT Feeback_lesshours FROM Feedback WHERE Assignment_Name='%s'", Assignment_Name)
 Less_hours <- fetch(dbSendQuery(mydb,sql),n=-1)
 
-sql <- sprintf("SELECT GPA FROM Student WHERE email='%s'", )
+sql <- sprintf("SELECT GPA FROM Student WHERE email='%s'", email)
 GPA <- fetch(dbSendQuery(mydb,sql),n=-1)
 
+sql <- sprintf("SELECT Year FROM Student WHERE email='%s'", email)
+Year <- fetch(dbSendQuery(mydb,sql),n=-1)
 
+sql <- sprintf("SELECT Major FROM Student WHERE email='%s'", email)
+Major <- fetch(dbSendQuery(mydb,sql),n=-1)
 
+Query <- SELECT (SELECT A.Suggested_Study_Time) + (SELECT F.Feedback_morehours) - (SELECT F.Feedback_lesshours) AS total_time, S.GPA, S.Year, S.Major 
 FROM Student S, Feedback F, Assignment A
 WHERE A.Assignment_Name = F.Assignment_Name AND S.email = F.email
 #Total_time <- Suggested_Study_Time + Feedback_morehours – Feedback_lesshours
@@ -25,5 +29,9 @@ Kmeans_model <- kmeans(df, cluster = 6, nstart = 10)
 aggregate(df, by = list(cluster=Kmeans_model$cluster), mean)
 Cluster_means <- Kmeans_model$centers
 
-
+all_cons <- dbListConnections(MySQL())
+for (mydb in all_cons){
+  dbDisconnect(mydb)
+#disconnect from database
+}
 
