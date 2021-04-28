9a301f26534d78e7fde9f6373fea57255b702ee0
Shailendra1811 <79381316+Shailendra1811@users.noreply.github.com>
2021-04-28 17:10:22 -0400

    Update GPA_vs_HrsPlots.R

--- Matching 2021/GPA_vs_HrsPlots.R
+++ Matching 2021/GPA_vs_HrsPlots.R
@@ -4,7 +4,27 @@ mydb <- dbConnect(MySQL(), user = 'g1117490', password = 'iegroup9', dbname = 'g
 on.exit(dbDisconnect(mydb))
 
 #database connection
-sql <- sprintf("SELECT GPA, YEAR FROM Student")
-Student_info <- fetch(dbSendQuery(mydb,sql),n=-1) 
 
-hist(Student_info)
+Year1 <- 'Freshman'
+Year2 <- 'Sophomore'
+Year3 <- 'Junior'
+Year4 <- 'Senior'
+
+sql <- sprintf("SELECT GPA FROM Student WHERE Year = '%s'", Year1)
+Freshmen <- fetch(dbSendQuery(mydb,sql),n=-1)
+
+
+sql <- sprintf("SELECT GPA FROM Student WHERE Year = '%s'", Year2)
+sophomore <- fetch(dbSendQuery(mydb,sql),n=-1)
+
+
+sql <- sprintf("SELECT GPA FROM Student WHERE Year = '%s'", Year3)
+junior <- fetch(dbSendQuery(mydb,sql),n=-1)
+
+
+sql <- sprintf("SELECT GPA FROM Student WHERE Year = '%s'", Year4)
+senior <- fetch(dbSendQuery(mydb,sql),n=-1)
+
+boxplot(c(Freshmen, sophomore, junior, senior),names = c("Freshmen", "Sophomore", "Junior", "Senior" ),  main = "GPA Distribution by Grade Year")
+
+
