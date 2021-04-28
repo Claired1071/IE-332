5ddc33d8c4b8cf7dc95ccbb0b3a60c0f2ad6d2f9
cmhancock31 <78760815+cmhancock31@users.noreply.github.com>
2021-04-28 15:55:08 -0400

    Update GPA_vs_HrsPlots.R

--- Matching 2021/GPA_vs_HrsPlots.R
+++ Matching 2021/GPA_vs_HrsPlots.R
@@ -1,1 +1,10 @@
+require("RMySQL")
+library(RMySQL)
+mydb <- dbConnect(MySQL(), user = 'g1117490', password = 'iegroup9', dbname = 'g1117490', host = 'mydb.itap.purdue.edu')
+on.exit(dbDisconnect(mydb))
 
+#database connection
+sql <- sprintf("SELECT GPA, YEAR FROM Student")
+Student_info <- fetch(dbSendQuery(mydb,sql),n=-1) 
+
+hist(Student_info)
